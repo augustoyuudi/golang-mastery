75415afd5098753c106937db209574c060694a63
augustoyuudi <augustoyuudi@gmail.com>
2024-02-03 09:30:09 -0300

    feat: implement basic auth

--- auth/basic/main.go
+++ auth/basic/main.go
@@ -8,10 +8,19 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.GET("/resource", func(c *gin.Context) {
+	r.GET("/resource", gin.BasicAuth(gin.Accounts{
+		"admin": "secret",
+	}), func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"data": "resource data",
 		})
 	})
 	r.Run() // Listen and serve on 0.0.0.0:8080 (for Windows "localhost:8080")
 }
+
+/*
+Can be tested with:
+
+curl -X GET "localhost:8080/resource" \
+	-u "admin:secrets"
+*/
